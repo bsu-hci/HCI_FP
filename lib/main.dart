ed2e6413e79491b2a5b6f8c326b1d82ab1a0a93f
Austin <afoster@bsu.edu>
2020-11-23 14:18:11 -0500

    Updates main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,153 @@
 import 'package:flutter/material.dart';
-
+ 
 void main() {
   runApp(MyApp());
 }
-
+ 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'HCI FP',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Build Week 1'),
+      home: MyHomePage(title: 'Build Week 2'),
     );
   }
 }
-
+ 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
-
+ 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
+ 
 class _MyHomePageState extends State<MyHomePage> {
-
+  Widget _participantCard() {
+    return Container(
+      alignment: Alignment.center,
+      height: 1250,
+      width: 1500,
+      color: Colors.blue[50],
+      child: Text("Participant Content will go here"),
+    );
+  }
+  
   Widget _commentButton() {
-    return Padding (
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: RaisedButton(
-      padding: const EdgeInsets.all(20.0),
+    return RaisedButton(
       onPressed: () {
         print("Uploads Comment");
       },
       child: const Text(
-      'Comment',
-      style: TextStyle(fontSize: 30),
+      'Upload Comment',
+      style: TextStyle(fontSize: 20),
       ),
-    )
     );
   }
-
   Widget _starButton() {
-    return Padding (
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: RaisedButton(
-      padding: const EdgeInsets.all(20.0),
+    return RaisedButton(
       onPressed: () {
-        print("Uploads Star Rating");
+        print("Uploads Start Rating");
       },
       child: const Text(
-      'Star Rating',
-      style: TextStyle(fontSize: 30),
+      'Upload Star Rating',
+      style: TextStyle(fontSize: 20),
       ),
-    )
     );
   }
-
   Widget _videoButton() {
-    return Padding (
-      padding: const EdgeInsets.symmetric(vertical: 20.0),
-      child: RaisedButton(
-      padding: const EdgeInsets.all(20.0),
+    return RaisedButton(
       onPressed: () {
-        print("Uploads Video Response");
+        print("Uploads Video");
       },
       child: const Text(
-      'Video',
-      style: TextStyle(fontSize: 30),
+      'Upload Video',
+      style: TextStyle(fontSize: 20),
       ),
+    );
+  }
+ 
+  Widget _commentCard() {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.blue,
+        height: 250,
+        child: Column(
+          children: [
+            TextField(
+              textAlign: TextAlign.center,
+              decoration: const InputDecoration(hintText: "Enter your comment here"),
+            ),
+            Spacer(),
+            _commentButton()
+          ]
+        ),
+      )
+    );
+  }
+ 
+  Widget _starCard() {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.blue,
+        height: 125,
+        child: Column(
+          children: [
+            TextField(
+              textAlign: TextAlign.center,
+              decoration: const InputDecoration(hintText: "Enter your star rating here (1-5)"),
+            ),
+            Spacer(),
+            _starButton()
+          ]
+        ),
+      )
+    );
+  }
+ 
+  Widget _videoCard() {
+    return Expanded(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        color: Colors.blue,
+        height: 125,
+        child: Column(
+          children: [
+            TextField(
+              textAlign: TextAlign.center,
+              decoration: const InputDecoration(hintText: "Enter a video URL here"),
+            ),
+            Spacer(),
+            _videoButton()
+          ]
+        ),
+      )
+    );
+  }
+ 
+  Widget _responseSection() {
+    return Container(
+      width: 600,
+      height: 500,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _commentCard(),
+          SizedBox(height: 20),
+          _starCard(),
+          SizedBox(height: 20),
+          _videoCard()
+      ],
     )
     );
   }
-
-  Widget _commentSection() {
-    return TextField(
-      textAlign: TextAlign.center,
-      decoration: const InputDecoration(hintText: "Enter your comment here"),
-    );
-  }
-
-  Widget _starSection() {
-    return TextField(
-      textAlign: TextAlign.center,
-      decoration: const InputDecoration(hintText: "Enter your star rating here (1-5)"),
-    );
-  }
-
-  Widget _videoSection() {
-    return Text("Video selection goes here");
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -102,21 +155,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Container(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("This Section would be underneath a users content."),
-              _commentSection(),
-              _commentButton(),
-              _starSection(),
-              _starButton(),
-              _videoSection(),
-              _videoButton()
+        child: SingleChildScrollView(
+        child: Column(
+            children: <Widget>[
+              SizedBox(height: 30),
+              _participantCard(),
+              SizedBox(height: 50),
+              _responseSection(),
             ],
-          ),
-        )
+          )
         ),
+      )
       );
   }
 }
+
